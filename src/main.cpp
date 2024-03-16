6a1f4fac2798bc0ea2847c778ca566304a235977
Capeling <113705504+Capeling@users.noreply.github.com>
2024-03-17 10:08:52 +1030

    this is why you test stuff kids

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ class $modify(PlayLayer) {
 bool myDrawCircle(CCDrawNode* ins, CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor) { //thanks TheSillyDoggo (https://github.com/TheSillyDoggo/GeodeMenu/blob/main/src/Hacks/SolidWaveTrail.cpp)
     if (typeinfo_cast<HardStreak*>(ins))
     {
-            if(!Mod::get()->getSettingValue<bool>("noWave")) return true;
+            if(Mod::get()->getSettingValue<bool>("noWave")) return true;
 
             if(Mod::get()->getSettingValue<bool>("solid")) {
                 if (fillColor.r >= 1.0f && fillColor.g >= 1.0f && fillColor.b >= 1.0f && ins->getColor() != ccc3(255, 255, 255))
