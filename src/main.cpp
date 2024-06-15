c3d35a9e0374428c680dd968326b8715a70ce926
Capeling <113705504+Capeling@users.noreply.github.com>
2024-06-15 16:37:56 +0930

    a

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@ class $modify(PlayLayer) {
 	}
 };
 
+#ifndef GEODE_IS_MAC
 bool myDrawCircle(CCDrawNode* ins, CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor) { //thanks TheSillyDoggo (https://github.com/TheSillyDoggo/GeodeMenu/blob/main/src/Hacks/SolidWaveTrail.cpp)
     if (typeinfo_cast<HardStreak*>(ins))
     {
@@ -60,3 +61,4 @@ $execute {
         tulip::hook::TulipConvention::Thiscall
     );
 }
+#endif
